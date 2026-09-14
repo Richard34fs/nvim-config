ca1d33fc6e203d1b04fee7ca96e22ddcaad78256
Richard Souza <104803256+Richard34fs@users.noreply.github.com>
2026-09-14 03:18:48 -0300

    Add Vim configuration options in options.lua

---
+++ lua/options.lua
@@ -0,0 +1,15 @@
+vim.o.number = true
+vim.o.backspace = '2'
+vim.o.showcmd = true
+vim.o.autowrite = true
+vim.o.cursorline = true
+vim.o.autoread = true
+vim.o.undofile = true
+vim.o.tabstop = 2
+vim.o.shiftround = true
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.shell = "zsh"
+vim.o.clipboard = "unnamedplus"
+vim.o.scrolloff = 4
+vim.o.termguicolors = true
